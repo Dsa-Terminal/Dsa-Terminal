a0c1f7143df66af825e542e4ebd50ccc329c1109
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-09-22 19:24:19 -0300

    Update Terminal.py

--- Terminal.py
+++ Terminal.py
@@ -1,3 +1,26 @@
+"""
+MIT License
+
+Copyright (c) 2020 Dsa-Terminal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 # Dsa Terminal codigo-fonte
 __version__ = '1.0.4'
 # Importando modulos
@@ -192,6 +215,10 @@ while True:
             print('nano [arquivo]        Dsa Terminal E-ditor')
             print('ping [parametros]     Opções de rede remota')
             print('./[shell script]      Executa shell script')
+            print('block                 Protetor de tela')
+        elif cmd == 'block':
+            startfile('Bubbles.scr')
+            continue
         elif cmd == '':
             for d in range(0, 1):
                 continue
