6eac165ee06cf0c2ffea56cd76a0b7ec6999e7c7
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-09-24 10:44:06 -0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
-git init
-git pull https://github.com/Dsa-Terminal/Dsa-Terminal.git master
+bin\git.exe init
+bin\git.exe pull https://github.com/Dsa-Terminal/Dsa-Terminal.git master
 pip install rich
 pip install tqdm
 python Terminal.py
