165f76c194cb68ee0d543c96cc17ed62e955b77e
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-09-24 13:52:33 -0300

    Shell adds

---
+++ Lib32/__init__.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+function foo() {
+    clear
+    echo "Ferramentas do sistema @root Dsa Terminal"
+    echo "O Acesso do usuario root não precisa ser solicitado para o uso"
+}
+foo
+ls
