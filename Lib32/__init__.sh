590430400b6a9f69ae4c4d77cf324b3b309adb4d
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-09-24 15:10:01 -0300

    Clear up

--- Lib32/__init__.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-function foo() {
-    clear
-    echo "Ferramentas do sistema @root Dsa Terminal"
-    echo "O Acesso do usuario root não precisa ser solicitado para o uso"
-}
-foo
-ls
