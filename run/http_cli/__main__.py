be9520dd1a47a77a54f0ba8f358f27540931e980
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-10-01 17:23:40 -0300

    Http Client

---
+++ run/http_cli/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""The main entry point. Invoke as `http' or `python -m httpie'.
+
+"""
+import sys
+
+def main():
+    try:
+        from httpie.core import main
+        exit_status = main()
+    except KeyboardInterrupt:
+        from httpie.status import ExitStatus
+        exit_status = ExitStatus.ERROR_CTRL_C
+
+    sys.exit(exit_status.value)
+
+
+if __name__ == '__main__':
+    main()
