7d458af3555e84ed3a208ff4c0d7dbcef8063f92
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-10-10 17:06:28 -0300

    test

---
+++ run/tests/iPXE.py
@@ -0,0 +1,9 @@
+"""
+iPXE para Dsa Terminal serviço de controle de Boot recurso [BETA] em desenvolvimento
+para guias e suporte ao Pxe Rom!
+"""
+__version__ = 'BETA'
+from os import system
+from time import sleep
+system('cls')
+print('')
