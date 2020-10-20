4884d0a151a77dccdd4869fee30f7f0c0feb4c50
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-10-20 10:46:33 -0300

    Revert "EXCEPTIONS"
    This reverts commit 94ffa007d38ab6d04012e7b02c0ec34d4b769efb.

--- run/Script-Terminal/Lib/System.py
+++ run/Script-Terminal/Lib/System.py
@@ -18,7 +18,7 @@ def wait(seconds):
 	try:
 		a = int(seconds)
 	except:
-		raise TypeError(f'Traceback (TypeError - {seconds})')
+		print(f'Traceback (TypeError - {seconds})')
 	else:
 		sleep(a)
 	return True
