2dde7acc8593de60737fbb93fc5d5e43a65bf0b9
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-11-27 10:13:22 -0300

    Update regedit.py

--- usr/regedit.py
+++ usr/regedit.py
@@ -4,7 +4,7 @@ class Start_Terminal:
     run_cmd = 'Terminal --login -i'
     protocols = ['MINGW64', 'MSYS']
     protocol = protocols[1]
-    dependences = ['flask', 'pygame', 'asyncio', 'sqlite3', 'sys', 'plataform', 'socket',
+    dependences = ['pygame', 'asyncio', 'sqlite3', 'sys', 'plataform', 'socket',
                    'os', 'random', 'time', 'getpass', 'tqdm', 'requests', 'rich.progress']
     module = '__main__'
     host = socket.gethostname()
